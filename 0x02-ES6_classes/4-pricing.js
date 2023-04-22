27c0f2ed38ce1f1dce5ee1b9179df17a32bb0054
Domengo <dominicsengo@gmail.com>
2023-04-22 04:35:49 +0300

    pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,32 +1,32 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-  constructor (amount, currency) {
+  constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
   }
 
-  get amount () {
+  get amount() {
     return this._amount;
   }
 
-  set amount (value) {
+  set amount(value) {
     this._amount = value;
   }
 
-  get currency () {
+  get currency() {
     return this._currency;
   }
 
-  set currenncy (value) {
+  set currenncy(value) {
     this._currency = value;
   }
 
-  displayFullPrice () {
+  displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
-  static convertPrice (amount, conversionRate) {
+  static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
