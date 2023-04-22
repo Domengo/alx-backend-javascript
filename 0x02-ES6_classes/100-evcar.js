13ac27c933b9a07fe3f75a6738c4ca3142495ac1
Domengo <dominicsengo@gmail.com>
2023-04-22 19:16:08 +0300

    symbols

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,34 +1,10 @@
+/* eslint-disable */
 import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
-  }
-
-  // Getters and setters for each attribute
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(value) {
-    this._motor = value;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(value) {
-    this._color = value;
-  }
-
-  get range() {
-    return this._range;
-  }
-
-  set range(value) {
-    this._range = value;
   }
 
   // Overriding cloneCar method to return instance of Car instead of EVCar
