3244c515043e2f45bece890aa823d13d6063deb0
Domengo <dominicsengo@gmail.com>
2023-04-28 16:16:40 +0300

    task 1-3

--- 0x04-TypeScript/task_3/js/crud.ts
+++ 0x04-TypeScript/task_3/js/crud.ts
@@ -1,14 +1,14 @@
-export function insertRow(row) {
+export function insertRow(row: any) {
     console.log('Insert row', row);
     return Math.floor(Math.random() * Math.floor(1000));
 }
 
-export function deleteRow(rowId) {
+export function deleteRow(rowId: any) {
     console.log('Delete row id', rowId);
     return;
 }
 
-export function updateRow(rowId, row) {
+export function updateRow(rowId:any, row:any) {
     console.log(`Update row ${rowId}`, row);
 
     return rowId;
--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,5 +1,5 @@
 import { RowID, RowElement } from './interface';
-import CRUD from './crud';
+import * as CRUD from './crud';
 
 const row: RowElement = {
     firstName: 'Guillaume',
@@ -8,7 +8,7 @@ const row: RowElement = {
 
 const newRowID: RowID = CRUD.insertRow(row);
 row.age = 23;
-const updatedRow: RowElement = row.age;
+const updatedRow: RowElement ={...row, age: 23};
 
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
