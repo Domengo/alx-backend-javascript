da081002fbf60a97dc4b078958ac48822838a973
Domengo <dominicsengo@mail.com>
2023-06-13 18:43:35 +0300

    a more compex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,10 +1,31 @@
 const http = require("http");
+const { countStudents } = require("./3-read_file_async");
 
 const app = http.createServer((req, res) => {
-  req.statusCode = 200;
-  req.setHeader("Content-Type", "text/plain");
-  res.end("Hello Holberton School!");
+  res.setHeader("Content-Type", "text/plain");
+
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.end("Hello Holberton School!\n");
+  } else if (req.url === "/students") {
+    res.statusCode = 200;
+    const database = "database.csv";
+    countStudents(database)
+      .then(() => {
+        res.end();
+      })
+      .catch((error) => {
+        res.statusCode = 500;
+        res.end(error.message);
+      });
+  } else {
+    res.statusCode = 404;
+    res.end("Not Found\n");
+  }
 });
+
 app.listen(1245, () => {
-  console.log("Server running");
+  console.log("Server is running on port 1245");
 });
+
+module.exports = app;
