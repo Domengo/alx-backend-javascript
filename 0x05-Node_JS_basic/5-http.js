9dba4f35c08265442ab07004d17a9e7d1c08312e
Domengo <dominicsengo@mail.com>
2023-06-13 18:00:18 +0300

    a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,10 +1,10 @@
-const http = require('http');
+const http = require("http");
 
 const app = http.createServer((req, res) => {
   req.statusCode = 200;
-  req.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  req.setHeader("Content-Type", "text/plain");
+  res.end("Hello Holberton School!");
 });
-app.listen('127.0.0.1', 1245, () => {
-  console.log('Server running');
+app.listen(1245, () => {
+  console.log("Server running");
 });
