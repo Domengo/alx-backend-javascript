0dbdd83d30dcf2ff3a81992247bae1ca2915b7d4
Domengo <dominicsengo@mail.com>
2023-06-16 16:32:32 +0300

    Async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,33 +1,9 @@
-/* eslint-disable jest/no-hooks */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable jest/prefer-expect-assertions */
-/* eslint-disable jest/valid-expect */
-const sinon = require('sinon');
-const { expect } = require('chai');
-// const { assert } = require('console');
-const getPaymentTokenFromAPI = require('./6-payment_token');
+const { getPaymentTokenFromAPI } = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  beforeEach('setting up spy for each test', () => {
-    sinon.spy(console, 'log');
-  });
-
-  afterEach('restore spy after each test', () => {
-    console.log.restore();
-  });
-  it('test the results of getPaymentTokenFromApi(false)', async () => {
-    await new Promise((done) => {
-      getPaymentTokenFromAPI(false);
-      expect(console.log.calledOnce).to.be.true;
-      done();
-    });
-  });
-
-  it('test the results of getPaymentTokenFromApi(true)', async () => {
-    await new Promise((done) => {
-      getPaymentTokenFromAPI(true);
-      expect(console.log.calledOnce).to.be.true;
-      done();
-    });
+  it('should return a resolved promise with the correct data when success is true', async () => {
+    expect.assertions(1);
+    const result = await getPaymentTokenFromAPI(true);
+    expect(result).toStrictEqual({ data: 'Successful response from the API' });
   });
 });
