b613fba3bdf965a5c0a301ed08aa59bf075b5fd9
Domengo <dominicsengo@mail.com>
2023-06-16 18:34:12 +0300

    Async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,14 +1,15 @@
+/* eslint-disable jest/no-test-callback */
 /* eslint-disable jest/prefer-expect-assertions */
 /* eslint-disable jest/valid-expect */
+const { describe, it } = require('mocha');
 const { expect } = require('chai');
-const { getPaymentTokenFromAPI } = require('./6-payment_token');
+const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('should return a resolved promise with the correct data when success is true', (done) => {
-    getPaymentTokenFromAPI(true)
-      .then((data) => {
-        expect(data).to.have.property('data');
-        done();
-      });
+  it('async testing with done callback', (done) => {
+    getPaymentTokenFromAPI(true).then((data) => {
+      expect(data).to.have.property('data');
+      done();
+    });
   });
 });
