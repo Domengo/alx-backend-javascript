8b0be6911ef9f053e564c95fc4163083da6fc41e
Domengo <dominicsengo@mail.com>
2023-06-16 18:29:36 +0300

    fixed

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,12 +1,14 @@
+/* eslint-disable jest/prefer-expect-assertions */
 /* eslint-disable jest/valid-expect */
 const { expect } = require('chai');
 const { getPaymentTokenFromAPI } = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('should return a resolved promise with the correct data when success is true', () => new Promise((done) => {
-    getPaymentTokenFromAPI(true).then((data) => {
-      expect(data).to.have.property('data');
-      done();
-    });
-  }));
+  it('should return a resolved promise with the correct data when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((data) => {
+        expect(data).to.have.property('data');
+        done();
+      });
+  });
 });
