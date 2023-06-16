919a485d26fffb1f3f7024d5e8963f6dabadfd95
Domengo <dominicsengo@mail.com>
2023-06-16 13:16:33 +0300

    hooks

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,4 +1,34 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable jest/no-hooks */
+/* eslint-disable jest/valid-expect */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable jest/prefer-expect-assertions */
 const sinon = require('sinon');
-const Utils = require('./utils');
 const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToAPI', () => {
+  let beforAfter;
+
+  beforeEach(() => {
+    if (!beforAfter) {
+      beforAfter = sinon.spy(console);
+    }
+  });
+
+  afterEach(() => {
+    beforAfter.log.resetHistory();
+  });
+
+  it('sendPaymentRequestToAPI with 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(beforAfter.log.calledWith('The total is: 120')).to.be.true;
+    expect(beforAfter.log.calledOnce).to.be.true;
+  });
+
+  it('sendPaymentRequestToAPI with 10, and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(beforAfter.log.calledWith('The total is: 20')).to.be.true;
+    expect(beforAfter.log.calledOnce).to.be.true;
+  });
+});
